6e070bb5830c52ece0f2ef989cb4c94c26eabca3
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2021-03-12 14:49:55 -0300

    Update indicators_mios.py

--- modules/indicators_mios.py
+++ modules/indicators_mios.py
@@ -17,7 +17,7 @@ Returns:
     copy of 'data' DataFrame with 'amplitud' column added, relativa al precio de cierre
 """
 def calcula_amplitud(data, high_col='<HIGH>', low_col='<LOW>', close_col='<CLOSE>'):
-  data['amplitud'] = (data[high_col] - data[low_col])/data[close_col]
+  data['<AMP>'] = (data[high_col] - data[low_col])/data[close_col]
   return data
 
 """
@@ -32,10 +32,7 @@ Returns:
 def estandariza_volumen(data, vol_col='<VOL>'):
   mean_vl = data['<VOL>'].mean()
   std_vl = data['<VOL>'].std()
-  data['vol_std'] = (data['<VOL>'] - mean_vl)/std_vl
-  data = data.drop('<VOL>', axis=1)
-  data['<VOL>'] = data['vol_std']
-  data = data.drop('vol_std', axis=1)
+  data['<VOL>'] = (data['<VOL>'] - mean_vl)/std_vl
   return data
 
 """
