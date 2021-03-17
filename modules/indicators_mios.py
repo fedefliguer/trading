c5cca7877afc85f35c83663af5c3016ac9812600
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2021-03-17 17:00:15 -0300

    Update indicators_mios.py

--- modules/indicators_mios.py
+++ modules/indicators_mios.py
@@ -73,9 +73,8 @@ Params:
 Returns:
     copy of 'data' DataFrame con columnas de número de días en (5, 30, 90, 180) los que el histograma MACD fue positivo, negativo e igual al del cierre
 """
-def calcula_canalidad_histog_macd(data):
-  list = [5, 30, 90, 180]
-  for ventana in list:
+def calcula_canalidad_histog_macd(data, histog_col='macd_histog', ventanas = [5, 30, 90, 180]):
+  for ventana in ventanas:
     i = 1
     data['lag_histog_1'] = data.histog.shift(1)
     colname_nu_1 = "nu_dias_histog_entre_5pc_%s" % (ventana)
